04021e5c2e802fc18622696c24a91fb6e4c0aea3
sugengprianto <55155451+sugengprianto@users.noreply.github.com>
2022-12-05 23:37:36 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Blog Masyarakat</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: '#',
   },
   chat: {
-    link: 'https://discord.com',
+    link: '#',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
